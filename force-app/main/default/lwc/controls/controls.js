fdadc025dfe7b9f4ab9e6be44523679fbcb57344
AbouOUAT <a_ouattara@outlook.fr>
2023-09-30 13:28:01 +0200

    Communicate from Child to Parent. Send Data

--- force-app/main/default/lwc/controls/controls.js
+++ force-app/main/default/lwc/controls/controls.js
@@ -3,13 +3,20 @@ import { LightningElement } from 'lwc';
 export default class Controls extends LightningElement {
 
     handleAdd() {
-        console.log ("Add A");
+        //console.log ("Add A");
         this.dispatchEvent(new CustomEvent('add'));
-        console.log ("Add B");
+        //console.log ("Add B");
       }
       handleSubtract() {
-        console.log ("subtract A");
+        //console.log ("subtract A");
         this.dispatchEvent(new CustomEvent('subtract'));
-        console.log ("subtract B");
+        //console.log ("subtract B");
+      }
+      //ici on a event en argument
+      handleMultiply(event) {
+        const factor = event.target.dataset.factor;
+        this.dispatchEvent(new CustomEvent('multiply', {
+          detail: factor
+        }));
       }
 }
